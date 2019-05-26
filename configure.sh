86d61e0bcb8795681e401bbbcb7e2ae5e7516e9c
copernicus231 <copernicus231@gmail.com>
2019-05-26 17:30:49 -0400

    fix files

--- configure.sh
+++ configure.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-USER_NAME=$1
 systemctl enable lightdm
-cp /tmp/lightdm.conf /etc/lightdm/lightdm.conf
-#touch $USER_NAME.txt
-mkdir /home/$USER_NAME
-sed -e "s/\${USERNAME}/${USER_NAME}/" /tmp/fstab > /etc/fstab
-parted /dev/sdb mklabel msdos
-parted /dev/sdb mkpart primary 0% 100%
-mkfs.ext4 /dev/sdb1
-mount -t ext4 /dev/sdb1 /home/${USER_NAME}
-useradd -M $USER_NAME
-echo "$USER_NAME:$USER_NAME"|chpasswd
-cp /etc/skel/.b* /home/$USER_NAME/
-chown -R $USER_NAME:$USER_NAME /home/$USER_NAME
-cp /tmp/.xinitrc /home/$USER_NAME/.xinitrc
+cp lightdm.conf /etc/lightdm/lightdm.conf
--- disksetup.sh
+++ disksetup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 USER_NAME=$1
+#DISK SETUP
 mkdir /home/$USER_NAME
-sed -e "s/\${USERNAME}/${USER_NAME}/" /tmp/fstab > /etc/fstab
+sed -e "s/\${USERNAME}/${USER_NAME}/" fstab > /etc/fstab
 parted /dev/sdb mklabel msdos
 parted /dev/sdb mkpart primary 0% 100%
 mkfs.ext4 /dev/sdb1
 mount -t ext4 /dev/sdb1 /home/${USER_NAME}
+
+#USER SETUP
 useradd -M $USER_NAME
 echo "$USER_NAME:$USER_NAME"|chpasswd
 cp /etc/skel/.b* /home/$USER_NAME/
 chown -R $USER_NAME:$USER_NAME /home/$USER_NAME
-cp /tmp/.xinitrc /home/$USER_NAME/.xinitrc
+cp .xinitrc /home/$USER_NAME/.xinitrc
